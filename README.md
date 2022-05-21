10465562bb4e765e7f674de7b0ba8d22fd405b8e
Johnson Fu <johnson.fu@gmail.com>
2022-05-21 22:59:15 +0800

    simple test

---
+++ README.md
@@ -0,0 +1,12 @@
+### React Native Study
+
+Need to use expo cli which is useful and provide more tools when compared to react CLI
+
+expo init project
+
+run the application: npm start
+
+use expo app to scan the qr code 
+
+need to install android studio
+
--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  // React Hook
+  const [message, setMessage] = useState('This is Testing!!!');
+
+  // On Press Handler
+  const onPressEventHandler = e => {
+    setMessage('Ah haaa !');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is testing!!!</Text>
+      <Text>{message}</Text>
+      <Button title='Tap me!' onPress={onPressEventHandler} />
       <StatusBar style="auto" />
     </View>
   );
